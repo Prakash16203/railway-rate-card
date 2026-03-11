fe5729d77c4adfebc835300f9945fc7fc4af86d0
Prakash16203 <admin@singhrailway.com>
2026-03-11 18:17:58 +0530

    app.py

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import os
 
 # Import models (db is defined here, but initialized later)
 from models import db, User, Rate, Vendor, RateCard, AccountGroup
-import os
-print(os.path.exists('static/Singh_logo.png'))  # Should print True
 # ────────────────────────────────────────────────
 # Load environment variables (harmless even without .env on Railway)
 # ────────────────────────────────────────────────
@@ -394,4 +392,5 @@ def logout():
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
+
 
